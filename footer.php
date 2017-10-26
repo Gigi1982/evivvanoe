7d63414d7561bc30ee4d15824f6264972b6da808
Gigi1303 <basile.luigi@gmail.com>
2017-10-26 17:56:23 +0200

    modifiche footer - aggiunta credits

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+			<footer style="height: 62px;" class="footer" role="contentinfo">
 
 				<!-- copyright -->
-				<p class="copyright">
+				<p style="margin:0;" class="copyright">
 					Evvivanoe - 2016 - C.F. 97803520010 - Via Giuseppe Grassi 16, Torino (TO)
 				</p>
+                <p style="margin:0; text-align:center; color:#fff; font-size:12px;" class="credits">
+                    developed by <a style="color:#fff; text-decoration:underline;" target="_blank" href="http://www.ikkai.it">ikkai</a>
+                </p>
 				<!-- /copyright -->
 
 			</footer>
@@ -17,13 +20,15 @@
 
 		<!-- analytics -->
 		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-77817505-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
 
 	</body>
 </html>
