e420624c91cc560066c3896d0fee08cde8627571
Gigi1982 <basile.luigi@gmail.com>
2018-10-09 16:02:06 +0200

    modifiche per form newsletter
    modifiche per form newsletter

--- homepage.php
+++ homepage.php
@@ -126,6 +126,99 @@ icon icon-right-open"></i> Tutte le news</a>
                     </div>
                 </div>
             </div>
+            <div class="section newsletter">
+                <div class="container">
+                    <div class="col-md-8 col-md-offset-2">
+                        <!-- Begin Mailchimp Signup Form -->
+                        <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+                        <style type="text/css">
+                            #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                        </style>
+                        <style type="text/css">
+                            #mc-embedded-subscribe-form input[type=checkbox]{display: inline; width: auto;margin-right: 10px;}
+                            #mergeRow-gdpr {margin-top: 20px;}
+                            #mergeRow-gdpr fieldset label {font-weight: normal;}
+                            #mc-embedded-subscribe-form .mc_fieldset{border:none;min-height: 0px;padding-bottom:0px;}
+                        </style>
+                        <div id="mc_embed_signup">
+                        <form action="https://evvivanoe.us5.list-manage.com/subscribe/post?u=a3892d7d047bc8d80249621ab&amp;id=ab7fc75d31" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+                            <h2>Iacriviti alla nostra newsletter</h2>
+                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                        <div class="mc-field-group">
+                            <label for="mce-EMAIL">Indirizzo mail  <span class="asterisk">*</span>
+                        </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-FNAME">Nome e cognome  <span class="asterisk">*</span>
+                        </label>
+                            <input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-MMERGE6">Città  <span class="asterisk">*</span>
+                        </label>
+                            <input type="text" value="" name="MMERGE6" class="required" id="mce-MMERGE6">
+                        </div>
+                        <div class="mc-field-group input-group">
+                            <strong>Formato Email </strong>
+                            <ul><li><input type="radio" value="html" name="EMAILTYPE" id="mce-EMAILTYPE-0"><label for="mce-EMAILTYPE-0">HTML</label></li>
+                        <li><input type="radio" value="text" name="EMAILTYPE" id="mce-EMAILTYPE-1"><label for="mce-EMAILTYPE-1">TESTO</label></li>
+                        </ul>
+                        </div>
+                        <div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+                            <div class="content__gdpr">
+                                <label>Marketing Permissions</label>
+                                <p>Evvivanoé utilizzerà le informazioni da te fornite in questo modulo per restare in contatto con te, aggiornarti sulle attività culturali e per renderti partecipe delle iniziative artistiche. Per favore facci sapere il modo in cui vorresti essere contattato:</p>
+                                <fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+                                <label class="checkbox subfield" for="gdpr_4101"><input type="checkbox" id="gdpr_4101" name="gdpr[4101]" value="Y" class="av-checkbox gdpr"><span>Email</span> </label><label class="checkbox subfield" for="gdpr_4105"><input type="checkbox" id="gdpr_4105" name="gdpr[4105]" value="Y" class="av-checkbox gdpr"><span>Direct Mail</span> </label><label class="checkbox subfield" for="gdpr_4109"><input type="checkbox" id="gdpr_4109" name="gdpr[4109]" value="Y" class="av-checkbox gdpr"><span>Customized online advertising</span> </label>
+                                </fieldset>
+                                <p>Puoi cambiare idea in qualsiasi momento e disiscriverti cliccando sul link di cancellazione al fondo di ogni email che ricevi o contattandoci su evvivanoe@evvivanoe.it. Tratteremo le tue informazioni con rispetto . Se vuoi conoscere il modo in cui trattiamo i dati personali puoi leggere l'informativa sulla privacy sul nostro sito. Cliccando qui sotto, dai il consenso al trattamento dei tuoi dati.</p>
+                            </div>
+                            <div class="content__gdprLegal">
+                                <p><a href="https://mailchimp.com/legal/" target="_blank"></a>We<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>use<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>Mailchimp<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>as<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>our<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>marketing<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>platform<a href="https://mailchimp.com/legal/" target="_blank"></a>. <a href="https://mailchimp.com/legal/" target="_blank"></a>By<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>clicking<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>below<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>to<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>subscribe<a href="https://mailchimp.com/legal/" target="_blank"></a>, <a href="https://mailchimp.com/legal/" target="_blank"></a>you<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>acknowledge<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>that<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>your<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>information<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>will<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>be<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>transferred<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>to<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>Mailchimp<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>for<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>processing<a href="https://mailchimp.com/legal/" target="_blank"></a>. <a href="https://mailchimp.com/legal/" target="_blank"></a>Learn<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>more<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>about<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>Mailchimp<a href="https://mailchimp.com/legal/" target="_blank"></a>'<a href="https://mailchimp.com/legal/" target="_blank"></a>s<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>privacy<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>practices<a href="https://mailchimp.com/legal/" target="_blank"></a> <a href="https://mailchimp.com/legal/" target="_blank"></a>here<a href="https://mailchimp.com/legal/" target="_blank"></a>.</p>
+                            </div>
+                        </div>
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a3892d7d047bc8d80249621ab_ab7fc75d31" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Iscriviti" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                        </div>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[6]='MMERGE6';ftypes[6]='text';fnames[2]='MMERGE2';ftypes[2]='birthday'; /*
+                         * Translated default messages for the $ validation plugin.
+                         * Locale: IT
+                         */
+                        $.extend($.validator.messages, {
+                               required: "Campo obbligatorio.",
+                               remote: "Controlla questo campo.",
+                               email: "Inserisci un indirizzo email valido.",
+                               url: "Inserisci un indirizzo web valido.",
+                               date: "Inserisci una data valida.",
+                               dateISO: "Inserisci una data valida (ISO).",
+                               number: "Inserisci un numero valido.",
+                               digits: "Inserisci solo numeri.",
+                               creditcard: "Inserisci un numero di carta di credito valido.",
+                               equalTo: "Il valore non corrisponde.",
+                               accept: "Inserisci un valore con un&apos;estensione valida.",
+                               maxlength: $.validator.format("Non inserire pi&ugrave; di {0} caratteri."),
+                               minlength: $.validator.format("Inserisci almeno {0} caratteri."),
+                               rangelength: $.validator.format("Inserisci un valore compreso tra {0} e {1} caratteri."),
+                               range: $.validator.format("Inserisci un valore compreso tra {0} e {1}."),
+                               max: $.validator.format("Inserisci un valore minore o uguale a {0}."),
+                               min: $.validator.format("Inserisci un valore maggiore o uguale a {0}.")
+                        });}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                        <!--End mc_embed_signup-->
+                    </div>
+                    
+                </div>
+                
+            </div>
 		</section>
 		<!-- /section -->
 	</main>
